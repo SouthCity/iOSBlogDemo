37f8194fd7d3afe297fecfdfbe83cab8e811a9ca
SouthCity <406719352@qq.com>
2016-03-14 18:31:46 +0800

    Merge branch 'master' into develop

---
+++ UITableViewAutolayoutCell/UITableViewAutolayoutCell/TableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  TableViewCell.h
+//  UITableViewAutolayoutCell
+//
+//  Created by chni on 16/3/14.
+//  Copyright © 2016年 孟家豪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewCell : UITableViewCell
+
+@property (nonatomic, weak) IBOutlet UILabel *contentLabel;
+
+@end
